b6c40cb4af6803a9770cc588caa337a546d6f8f0
krishnaprasanth7102 <kpts7102@gmail.com>
2024-03-06 14:43:08 +0530

    heloooo

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import file
+myapp=FastAPI()
+maman=file.Maman
+@myapp.get("/{maman_id}")
+def index(maman_id:int):
+    return maman[maman_id]
+@myapp.post("/add_maman")
+def add(name:str,age:int):
+    new_id=len(maman)+1
+    
+    new_maman={new_id :{"name":name,
+                         "age":age
+                 }
+               }
+    maman.update(new_maman)
+    return maman
---
+++ file.py
@@ -0,0 +1,16 @@
+Maman={
+    1:{
+        "name":"Surendran",
+        "age":46
+        },
+    2:{
+        "name":"vasuu",
+        "age":55
+        },
+    3:{
+        "name":"Raveendran",
+        "age":74
+    }    
+
+
+}
